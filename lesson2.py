aa35eba9e4fe93263d3ce4a0798cdb969e709fa6
Withhell <watherd29@gmail.com>
2023-09-01 20:18:18 +0300

    Added gener info about conditions

--- lesson2.py
+++ lesson2.py
@@ -1,3 +1,18 @@
 # git init -> create git repository 
-print('hello world')
-print('i dont understand')
+# print('hello world')
+# print('i dont understand')
+
+####################
+# v1 
+
+# n1 = 10  + 20 * 2 # оператор присвоєння, обробляється останнім
+# n2 = 20 
+
+# v2
+# n1, n2 = 10, 20 # множинне присвоєння
+
+# is_valid = False
+# print(is_valid)
+# print(not is_valid) # not -> inversion
+
+# print('hello' in 'hello world')
